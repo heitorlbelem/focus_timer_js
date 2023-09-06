096615fc11ec795f6e82f72ab40f88dde065eb3f
Heitor Belém <heitorlbelem98@gmail.com>
2023-09-06 12:23:21 -0300

    Add logic to change html element classes

---
+++ js/FocusTimer/actions.js
@@ -0,0 +1,16 @@
+import { state } from "./state.js"
+
+export function toggleRunning() {
+  state.isRunning = document.documentElement.classList.toggle("running")
+}
+
+export function set() {}
+
+export function reset() {
+  state.isRunning = false
+  document.documentElement.classList.remove("running")
+}
+
+export function toggleMusic() {
+  state.isMuted = document.documentElement.classList.toggle("music-on")
+}
