a97461009af403c1818cc0ef0a61ec85ccc87b6f
Heitor Belém <heitorlbelem98@gmail.com>
2023-09-06 12:35:36 -0300

    Creates countdown function

---
+++ js/FocusTimer/timer.js
@@ -0,0 +1,20 @@
+import { state } from "./state.js"
+import * as elements from "./elements.js"
+
+export function updateDisplay(minutes, seconds) {
+  minutes = minutes ?? state.minutes
+  seconds = seconds ?? state.seconds
+
+  elements.minutes.textContent = String(minutes).padStart(2, "0")
+  elements.seconds.textContent = String(seconds).padStart(2, "0")
+}
+
+export function countdown() {
+  if (!state.isRunning) {
+    return
+  }
+
+  console.log("iniciou")
+
+  setTimeout(() => countdown(), 1000)
+}
